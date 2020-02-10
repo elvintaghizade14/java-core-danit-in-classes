1f9b98bd672ceb7daf0594ba745d5c508b2629ba
Elvin Taghizade <elvintaghiyev184@gmail.com>
2020-02-10 19:04:27 +0400

    general commit

--- src/main/java/app/HelloWorldApp.java
+++ src/main/java/app/HelloWorldApp.java
@@ -1,9 +1,11 @@
 package app;
 
+import java.io.InputStream;
+
 public class HelloWorldApp {
 
     public static void main(String[] args) {
-
+        System.out.println("Hello, World!");
     }
 
 }
