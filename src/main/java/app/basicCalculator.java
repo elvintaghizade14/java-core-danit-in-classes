649ee1902bcbdd5f4d12b2c4198e351888001a67
Elvin Taghizade <elvintaghiyev184@gmail.com>
2020-02-10 19:00:16 +0400

    basic calculator solved (only addition) #4

--- src/main/java/app/basicCalculator.java
+++ src/main/java/app/basicCalculator.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class basicCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         //asking input for first number
         System.out.print("Please enter first number:   ");
         double first_num = input.nextDouble();
