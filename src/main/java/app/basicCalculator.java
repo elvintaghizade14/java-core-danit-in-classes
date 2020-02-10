c1fd6093e37454caa465b1fbdf108377a17feece
Elvin Taghizade <elvintaghiyev184@gmail.com>
2020-02-10 18:58:50 +0400

    adding comments :)

--- src/main/java/app/basicCalculator.java
+++ src/main/java/app/basicCalculator.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class basicCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        //asking input for first number
         System.out.print("Please enter first number:   ");
         double first_num = input.nextDouble();
 
+        //asking input for second number
         System.out.print("Please enter first number:   ");
         double second_num = input.nextDouble();
 
+        //adding numbers and printing on console
         System.out.print("Sum of " + first_num + " and " + second_num + " is: " +
                 (first_num + second_num));
     }
