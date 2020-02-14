253ba537c37bd76abaa2cd86575e7553800018b3
Elvin Taghizade <elvintaghiyev184@gmail.com>
2020-02-14 19:01:04 +0400

    task_4 commit No_#2 optimisation

---
+++ src/main/java/app/fillTwoDimensionalArray.java
@@ -0,0 +1,34 @@
+package app;
+
+public class fillTwoDimensionalArray {
+    public static void main(String[] args) {
+        // creating initial data
+        final int WIDTH = 20;
+        final int HEIGHT = 13;
+        final int[][] a = new int[HEIGHT][WIDTH];
+        final int MIN = 1;
+        final int MAX = (Math.min(WIDTH, HEIGHT) + 1) / 2;
+        // process
+        for (int level = MIN; level <= MAX; level++) {
+            for (int row = 0; row < HEIGHT; row++) {
+                for (int col = 0; col < WIDTH; col++) {
+                    if ((row + 1 == level && col + 1 >= level && WIDTH - col >= level) ||
+                            (HEIGHT - row == level && col + 1 >= level && WIDTH - col >= level) ||
+                            (row + 1 == level && col + 1 == level)
+                    ) a[row][col] = level;
+
+                }
+            }
+        }
+
+        // output
+        StringBuilder out = new StringBuilder();
+        for (int row = 0; row < HEIGHT; row++) {
+            for (int col = 0; col < WIDTH; col++) {
+                out.append(String.format("%d ", a[row][col]));
+            }
+            out.append("\n");
+        }
+        System.out.println(out);
+    }
+}
