1f05c734f22e49fe1afbd46c3482ae4a246e5cfc
Inara <vafamustafayeva4@gmail.com>
2020-02-17 20:26:05 +0400

    17/02/2020 warm_up #2 commit_#5

---
+++ src/main/java/lesson_06/StringFilter.java
@@ -0,0 +1,29 @@
+package lesson_06;
+
+public class StringFilter {
+    public static void main(String[] args) {
+        final String VOW = "AEOIU";
+        int range = 'Z' - 'A' + 1;
+        int start = 'A';
+        StringBuilder rand_s = new StringBuilder();
+        for (int i = 0; i < 30; i++) {
+            char letter = (char) (Math.random() * range + start +
+                    (Math.random() * 2) * 32);
+            rand_s.append(letter);
+        }
+        String random = rand_s.toString();
+        StringBuilder vowels = new StringBuilder();
+        StringBuilder consonants = new StringBuilder();
+        for (int i = 0; i < random.length(); i++) {
+            char c = random.charAt(i);
+            if (VOW.contains(String.valueOf(c)))
+                vowels.append(c);
+            else
+                consonants.append(c);
+        }
+
+        System.out.println(random);
+        System.out.println(vowels.toString());
+        System.out.println(consonants.toString());
+    }
+}
