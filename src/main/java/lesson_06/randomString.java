33128eb5fbec1a87d6f35e9edf434a0430872896
Inara <vafamustafayeva4@gmail.com>
2020-02-17 20:02:29 +0400

    17/02/2020 warm_up #2 commit_#4

--- src/main/java/lesson_06/randomString.java
+++ src/main/java/lesson_06/randomString.java
@@ -2,18 +2,34 @@ package lesson_06;
 
 public class randomString {
     public static void main(String[] args) {
+
+        final String VOW_CAP = "AEIOU";
+        final String VOW_LOW = "aeiou";
+
         StringBuilder randStr = new StringBuilder();
-        StringBuilder vowels = new StringBuilder();
-        StringBuilder consonants = new StringBuilder();
+        StringBuilder vowels_cap = new StringBuilder();
+        StringBuilder vowels_low = new StringBuilder();
+        StringBuilder consonants_cap = new StringBuilder();
+        StringBuilder consonants_low = new StringBuilder();
+
         for (int i = 0; i < 30; i++) {
-            int randNum = ((int) (Math.random() * 26 + 65));
-            randStr.append((char) randNum);
-            if (randNum == 65 || randNum == 69 || randNum == 73 || randNum == 79 || randNum == 85)
-                vowels.append((char) randNum);
-            else consonants.append((char) randNum);
+            int randNumCap = ((int) (Math.random() * 26 + 65));
+            int randNumLow = ((int) (Math.random() * 26 + 97));
+            randStr.append((char) randNumCap);
+            randStr.append((char) randNumLow);
+            if (VOW_CAP.contains(String.valueOf((char) randNumCap)))
+                vowels_cap.append((char) randNumCap);
+            else if (VOW_LOW.contains(String.valueOf((char) randNumLow)))
+                vowels_low.append((char) randNumLow);
+            else if (randNumCap >= 65 && randNumCap <= 90)
+                consonants_cap.append((char) randNumCap);
+            else
+                consonants_low.append((char) randNumLow);
         }
         System.out.println(randStr.toString());
-        System.out.println(vowels.toString());
-        System.out.println(consonants.toString());
+        System.out.println(vowels_cap);
+        System.out.println(vowels_low);
+        System.out.println(consonants_cap);
+        System.out.println(consonants_low);
     }
 }
