1a832cd258ec573586472dc291ebe06127de2f02
Elvin Taghizade <elvintaghiyev184@gmail.com>
2020-02-26 21:06:41 +0400

    26/02/2020 warmup_02 - commit_#04

---
+++ src/main/java/lesson_10/warmup_03/warmup_03.java
@@ -0,0 +1,32 @@
+package lesson_10.warmup_03;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class warmup_03 {
+
+  public Map<Character, Integer> countOfEachLetter(String s) {
+    HashMap<Character, Integer> listOfLetters = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+      listOfLetters.put(s.charAt(i), getCount(s.charAt(i), s));
+    }
+    return listOfLetters;
+  }
+
+  private int getCount(char c, String s) {
+    int counter = 0;
+    for (int j = 0; j < s.length(); j++) {
+      if (c == s.charAt(j)) counter++;
+    }
+    return counter;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine();
+
+    warmup_03 r = new warmup_03();
+    System.out.println(r.countOfEachLetter(s));
+  }
+}
