380672914e66258edacc3044cf06d9c34f020191
Elvin Taghizade <elvintaghiyev184@gmail.com>
2020-02-26 18:16:29 +0400

    26/02/2020 warmup_02 - commit_#02

--- src/main/java/lesson_10/warmup_02/warmup_02.java
+++ src/main/java/lesson_10/warmup_02/warmup_02.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class warmup_02 {
   public List<Integer> mergeLists(List<Integer> f, List<Integer> s) {
-    if(f == null && s == null) throw new IllegalArgumentException("Null argument is not valid!");
+    if(f == null || s == null) throw new IllegalArgumentException("Null argument is not valid!");
     HashSet<Integer> merged = new HashSet<>();
-    merged.addAll(Objects.requireNonNull(f));
+    merged.addAll(f);
     merged.addAll(s);
     return new ArrayList<>(merged);
   }
