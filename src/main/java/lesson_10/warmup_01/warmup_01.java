f846363f46af0e35c9d564885fd06107aef85c93
Elvin Taghizade <elvintaghiyev184@gmail.com>
2020-02-26 16:29:41 +0400

    26/02/2020 warmup_01 - testing with filter method for vowels commit_#02

--- src/main/java/lesson_10/warmup_01/warmup_01.java
+++ src/main/java/lesson_10/warmup_01/warmup_01.java
@@ -1,15 +1,18 @@
 package lesson_10.warmup_01;
 
 public class warmup_01 {
-  public static String filter(String origin) {
-    final String VOWELS = "AEIOU";
-    origin = origin.toUpperCase();
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < origin.length(); i++) {
-      if (!(VOWELS.contains(String.valueOf(origin.charAt(i))))) {
-        sb.append(origin.charAt(i));
+  public String filter(String origin) {
+    if (origin == null) throw new IllegalArgumentException("'null' is not a value!");
+    else {
+      final String VOWELS = "AEIOU";
+      origin = origin.toUpperCase();
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < origin.length(); i++) {
+        if (!(VOWELS.contains(String.valueOf(origin.charAt(i))))) {
+          sb.append(origin.charAt(i));
+        }
       }
+      return sb.toString();
     }
-    return sb.toString();
   }
 }
--- src/test/java/lesson_10/warmup_01/warmup_01Test.java
+++ src/test/java/lesson_10/warmup_01/warmup_01Test.java
@@ -5,18 +5,33 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class warmup_01Test {
+  final warmup_01 ref1 = new warmup_01();
 
   @Test
   void test1() {
     String expected = "HLL, WRLD!";
-    String actual = warmup_01.filter("Hello, World!");
+    String actual = ref1.filter("Hello, World!");
     assertEquals(expected, actual);
   }
 
   @Test
   void test2() {
     String expected = "";
-    String actual = warmup_01.filter("aeiou");
+    String actual = ref1.filter("aeiou");
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void test3() {
+    String expected = "";
+    String actual = ref1.filter("");
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void test4() {
+    String expected = "";
+    String actual = ref1.filter(null);
     assertEquals(expected, actual);
   }
 
