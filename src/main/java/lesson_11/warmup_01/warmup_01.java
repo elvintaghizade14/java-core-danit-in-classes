267371fad84dbf8c95078c7dfc7c10e8cd854f54
Elvin Taghizade <elvintaghiyev184@gmail.com>
2020-02-28 16:59:18 +0400

    lesson_11 commit_01 warmup_01

---
+++ src/main/java/lesson_11/warmup_01/warmup_01.java
@@ -0,0 +1,30 @@
+package lesson_11.warmup_01;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class warmup_01 {
+  public static void main(String[] args) {
+    String origin = "Hello World";
+    HashMap<Character, List<Integer>> map = new HashMap<>();
+
+    IntStream.range(0, origin.length()).forEach(i -> {
+      char c = origin.charAt(i);
+      List<Integer> positions = map.getOrDefault(c, new ArrayList<>());
+      positions.add(i);
+      map.put(c, positions);
+    });
+
+    ArrayList<Character> chars = new ArrayList<>(map.keySet());
+    Collections.sort(chars);
+
+    for (char item : chars) {
+      map.forEach((c, n) -> {
+        if (item == c) System.out.printf("Letter: '%c', Count: %d, Positions:%s\n", c, n.size(), n);
+      });
+    }
+  }
+}
