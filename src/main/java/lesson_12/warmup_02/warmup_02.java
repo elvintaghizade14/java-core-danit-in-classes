90c9976cfc378dc45231cd2f46b9ef52eaacf728
Elvin Taghizade <elvintaghiyev184@gmail.com>
2020-03-02 18:35:46 +0400

    lesson_12 warmup_02 commit_02

--- src/main/java/lesson_12/warmup_02/warmup_02.java
+++ src/main/java/lesson_12/warmup_02/warmup_02.java
@@ -1,18 +1,22 @@
 package lesson_12.warmup_02;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class warmup_02 {
   public static void main(String[] args) {
     List<String> subjects = Arrays.asList("Noel", "The cat", "The dog");
-    List<String> verbs =    Arrays.asList("wrote", "chased", "slept on");
-    List<String> objects =  Arrays.asList("the book","the ball","the bed");
+    List<String> verbs = Arrays.asList("wrote", "chased", "slept on");
+    List<String> objects = Arrays.asList("the book", "the ball", "the bed");
 
-    for (String subject : subjects)
-      for (String verb : verbs)
-        for (String object : objects)
-          System.out.printf("%s %s %s.\n", subject, verb, object);
+//    for (String subject : subjects)
+//      for (String verb : verbs)
+//        for (String object : objects)
+//          System.out.printf("%s %s %s.\n", subject, verb, object);
+
+    StringBuilder sb = new StringBuilder();
+    subjects.forEach(s -> verbs.forEach(v -> objects.forEach(o ->
+            sb.append(s).append(' ').append(v).append(' ').append(o).append('\n'))));
+    System.out.println(sb.toString());
   }
 }
