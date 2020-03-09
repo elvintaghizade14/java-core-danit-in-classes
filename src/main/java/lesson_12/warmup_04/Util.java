41c3ae47821f2482f630299f8bbb59b60153d26c
Elvin Taghizade <elvintaghiyev184@gmail.com>
2020-03-09 11:29:46 +0400

    lesson_12 warmup_03 commit_02

---
+++ src/main/java/lesson_12/warmup_04/Util.java
@@ -0,0 +1,22 @@
+package lesson_12.warmup_04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Util {
+  public static List<Integer> convertStrToInt(List<String> listStr) {
+    ArrayList<Integer> result = new ArrayList<>();
+    for (String item : listStr) {
+      result.add(Integer.parseInt(item));
+    }
+    return result;
+  }
+
+  public static List<String> convertIntToStr(List<Pair> listPair) {
+    ArrayList<String> result = new ArrayList<>();
+    for (Pair item : listPair) {
+      result.add(String.format("%s:%s", item.left, item.right));
+    }
+    return result;
+  }
+}
--- src/main/java/lesson_12/warmup_04/fileIO.java
+++ src/main/java/lesson_12/warmup_04/fileIO.java
@@ -1,4 +1,5 @@
-package lesson_12.warmup_03;
+package lesson_12.warmup_04;
+
 
 import java.io.*;
 import java.util.ArrayList;
@@ -8,20 +9,23 @@ import java.util.stream.Collectors;
 public class fileIO {
   public static void main(String[] args) throws IOException {
 
-    List<Integer> randomsInt = generate100(100, -30, 30);
-    List<String> randomsStr = Util.convertIntToStr(randomsInt);
+    List<Pair> pairs = generate100(100, -30, 30);
+    List<String> randomsStr = Util.convertIntToStr(pairs);
     write(randomsStr, "file1.txt");
     List<String> randoms2Str = read("file1.txt");
     List<Integer> randoms2Int = Util.convertStrToInt(randoms2Str);
     List<Integer> sorted_randomsInt = sort(randoms2Int);
-    List<String> sorted_randomsStr = Util.convertIntToStr(sorted_randomsInt);
-    write(sorted_randomsStr, "file2.txt");
+//    List<String> sorted_randomsStr = Util.convertIntToStr(sorted_randomsInt);
+//    write(sorted_randomsStr, "file2.txt");
   }
 
-  private static List<Integer> generate100(int count, int min, int max) {
-    ArrayList<Integer> list = new ArrayList<>();
-    for (int i = 0; i < count; i++) list.add((int) (Math.random() * (max - min + 1) + min));
-    return list;
+  private static ArrayList<Pair> generate100(int count, int min, int max) {
+    ArrayList<Pair> result = new ArrayList<>();
+    for (int i = 0; i < count; i++) {
+      Pair pairs = new Pair((int) (Math.random() * (max - min + 1) + min), (int) (Math.random() * (max - min + 1) + min));
+      result.add(pairs);
+    }
+    return result;
   }
 
   private static List<String> read(String name) throws IOException {
@@ -37,10 +41,10 @@ public class fileIO {
     return data.stream().sorted().collect(Collectors.toList());
   }
 
-  private static void write(List<String> processed, String fileName) throws IOException {
+  private static void write(List<String> processedL, String fileName) throws IOException {
     File ofile = new File(fileName);
     BufferedWriter bw = new BufferedWriter(new FileWriter(ofile));
-    for (String line : processed) {
+    for (String line : processedL) {
       bw.write(line);
       bw.write("\n");
     }
