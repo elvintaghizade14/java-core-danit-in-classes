8cb9bec9cc37286a17ca836cecc172cecf6a542b
Elvin <elviuntaghiyev1842gmail.com>
2020-03-23 17:06:16 +0400

    ex_#1;FactorialApp;commit_#2

--- src/main/java/module_02/lesson_03/FactorialApp.java
+++ src/main/java/module_02/lesson_03/FactorialApp.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class FactorialApp {
   private static int fact(int n) {
-    return (n == 1 || n ==0) ? 1 : n * fact(n - 1);
+    return (n == 1 || n == 0) ? 1 : n * fact(n - 1);
   }
 
   public static void main(String[] args) {
