d52943ef558070892cdc7d21aa3316450f9f8274
Elvin <elviuntaghiyev1842gmail.com>
2020-03-23 17:21:05 +0400

    ex_#1;FibonacciApp;commit_#1

--- src/main/java/module_02/lesson_03/FibonaciApp.java
+++ src/main/java/module_02/lesson_03/FibonaciApp.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class FibonaciApp {
 
-  public static void main(String[] args) {
-    System.out.print("Enter a number:  ");
-    System.out.printf("Factorial: %d\n", fib(new Scanner(System.in).nextInt()));
-  }
-
   private static int fib(int num) {
     if (num == 0) throw new IllegalArgumentException("Wrong value!");
     return (num == 1 || num == 2) ? 1 : fib(num - 1) + fib(num - 2);
+  }
+
+  public static void main(String[] args) {
+    System.out.print("Enter a number:  ");
+    System.out.printf("Factorial: %d\n", fib(new Scanner(System.in).nextInt()));
   }
 }
