4d65ac284d927b72aa7a63186a63dfbaa6f3155a
Elvin <elviuntaghiyev1842gmail.com>
2020-03-23 17:25:43 +0400

    ex_#1;FibonacciApp;commit_#1

--- src/main/java/module_02/lesson_03/FibonacciApp.java
+++ src/main/java/module_02/lesson_03/FibonacciApp.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class FibonacciApp {
 
-  private static int fib(int num) {
+  private static int fibo(int num) {
     if (num == 0) throw new IllegalArgumentException("Wrong value!");
-    return (num == 1 || num == 2) ? 1 : fib(num - 1) + fib(num - 2);
+    return (num == 1 || num == 2) ? 1 : fibo(num - 1) + fibo(num - 2);
   }
 
   public static void main(String[] args) {
     System.out.print("Enter a number:  ");
-    System.out.printf("Fibonacci: %d\n", fib(new Scanner(System.in).nextInt()));
+    System.out.printf("Fibonacci: %d\n",
+            fibo(new Scanner(System.in).nextInt()));
   }
 }
