36e22a850ce6c18735d5851714853bae529f4c0f
Elvin <elviuntaghiyev1842gmail.com>
2020-03-23 16:17:32 +0400

    warmup_#1; commit_#1

--- src/main/java/module_02/lesson_03/warmup_01.java
+++ src/main/java/module_02/lesson_03/warmup_01.java
@@ -4,16 +4,21 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class warmup_01 {
+  private static int index = 0;
 
   public static int sum(int[] data, int index) {
     if (data.length <= index) return 0;
     return data[index] + sum(data, index+1);
   }
 
+  public static int sum(int[] data) {
+    return sum(data, index++);
+  }
+
   public static void main(String[] args) {
     // our random data
-    int[] d = new Random().ints(1, 10).limit(10).toArray();
+    int[] d = new Random().ints(1, 10).limit(3).toArray();
     System.out.printf("Source array: %s\n", Arrays.toString(d));
-    System.out.printf("Sum: %d\n", sum(d, 0));
+    System.out.printf("Sum: %d\n", sum(d));
   }
 }
