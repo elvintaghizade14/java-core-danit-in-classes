a07e12ebae6f724929decc1907db72606857f447
Elvin <elviuntaghiyev1842gmail.com>
2020-03-24 17:03:57 +0400

    commit_#1

--- src/main/java/module_02/lesson_04/warmups/GCDApp.java
+++ src/main/java/module_02/lesson_04/warmups/GCDApp.java
@@ -1,16 +1,9 @@
 package module_02.lesson_04.warmups;
 
 public class GCDApp {
-  public static int gcd(int a, int b) {
-    if (a == b) return a;
-    else {
-      int max = Math.max(a, b);
-      if (a == max) return gcd(a-b, b);
-      else return gcd(a, b-a);
-    }
-  }
 
-  public static void main(String[] args) {
-    System.out.println(gcd(24, 36));
+  public int gcd(int a, int b) {
+    if (a == b) return a;
+    return a == Math.max(a, b) ? gcd(a-b, b) : gcd(a, b-a);
   }
 }
---
+++ src/test/java/module_02/lesson_04/warmups/GCDAppTest.java
@@ -0,0 +1,36 @@
+package module_02.lesson_04.warmups;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GCDAppTest {
+  private GCDApp g;
+
+  @BeforeEach
+  void bef() {
+    g = new GCDApp();
+  }
+
+  @Test
+  void gcd1() {
+    assertEquals(1, g.gcd(7,5));
+  }
+
+  @Test
+  void gcd2() {
+    assertEquals(5, g.gcd(20,5));
+  }
+
+  @Test
+  void gcd3() {
+    assertEquals(12, g.gcd(36, 24));
+  }
+
+  @Test
+  void gcd4() {
+    assertEquals(3, g.gcd(3, 9));
+  }
+
+}
