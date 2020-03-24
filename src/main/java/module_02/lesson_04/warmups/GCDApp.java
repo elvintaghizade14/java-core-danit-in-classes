6f6ac9a0beb211dbf46556dfd7a770e3a6ed4f74
Elvin <elviuntaghiyev1842gmail.com>
2020-03-24 17:04:08 +0400

    commit_#1

--- src/main/java/module_02/lesson_04/warmups/GCDApp.java
+++ src/main/java/module_02/lesson_04/warmups/GCDApp.java
@@ -1,9 +1,8 @@
 package module_02.lesson_04.warmups;
 
 public class GCDApp {
-
   public int gcd(int a, int b) {
     if (a == b) return a;
-    return a == Math.max(a, b) ? gcd(a-b, b) : gcd(a, b-a);
+    return a == Math.max(a, b) ? gcd(a - b, b) : gcd(a, b - a);
   }
 }
