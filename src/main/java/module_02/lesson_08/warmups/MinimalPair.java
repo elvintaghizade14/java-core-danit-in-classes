16b881779cdcc0e247de067a8b6610c32a90a831
Elvin <elviuntaghiyev1842gmail.com>
2020-04-01 18:19:10 +0400

    commit_#1

--- src/main/java/module_02/lesson_08/warmups/MinimalPair.java
+++ src/main/java/module_02/lesson_08/warmups/MinimalPair.java
@@ -6,29 +6,29 @@ import java.util.stream.Collectors;
 
 public class MinimalPair {
   public static void main(String[] args) {
-    List<Integer> numbers = new Random().ints(0, 51)
+    List<Integer> data = new Random().ints(0, 51)
             .limit(30)
             .boxed()
             .collect(Collectors.toList());
-
+    System.out.println(data);
     int index = 0;
-    int num1 = numbers.get(0);
-    int num2 = numbers.get(1);
+    int num1 = data.get(0);
+    int num2 = data.get(1);
     int sum = num1 + num2;
 
-    for (int i = 0; i < numbers.size() - 1; i++) {
-      int nextSum = numbers.get(i) + numbers.get(i);
-      if (sum < nextSum) {
+    for (int i = 0; i < data.size() - 1; i++) {
+      int nextSum = data.get(i) + data.get(i);
+      if (sum > nextSum) {
         sum = nextSum;
-        num1 = numbers.get(i);
-        num2 = numbers.get(i + 1);
-        index = numbers.indexOf(num1);
+        num1 = data.get(i);
+        num2 = data.get(i + 1);
+        index = data.indexOf(num1);
       }
     }
 
     System.out.printf("index of pair: %d\n", index);
     System.out.printf("Number 1:      %d\n", num1);
     System.out.printf("Number 2:      %d\n", num2);
-    System.out.printf("Sum:           %d\n", sum);
+    System.out.printf("Sum:           %d\n", num1+num2);
   }
 }
