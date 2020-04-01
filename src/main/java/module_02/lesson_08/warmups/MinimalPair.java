8d02fb51cd4b2246291a8d758ed973ad3d3df0b8
Elvin <elviuntaghiyev1842gmail.com>
2020-04-01 18:59:11 +0400

    commit_#1

--- src/main/java/module_02/lesson_08/warmups/MinimalPair.java
+++ src/main/java/module_02/lesson_08/warmups/MinimalPair.java
@@ -10,25 +10,26 @@ public class MinimalPair {
             .limit(30)
             .boxed()
             .collect(Collectors.toList());
-    System.out.println(data);
+
     int index = 0;
     int num1 = data.get(0);
     int num2 = data.get(1);
     int sum = num1 + num2;
 
-    for (int i = 0; i < data.size() - 1; i++) {
+    for (int i = 1; i < data.size() - 1; i++) {
       int nextSum = data.get(i) + data.get(i);
       if (sum > nextSum) {
-        sum = nextSum;
+        index = i;
         num1 = data.get(i);
         num2 = data.get(i + 1);
-        index = data.indexOf(num1);
+        sum = nextSum;
       }
     }
 
+    System.out.println(data);
     System.out.printf("index of pair: %d\n", index);
     System.out.printf("Number 1:      %d\n", num1);
     System.out.printf("Number 2:      %d\n", num2);
-    System.out.printf("Sum:           %d\n", num1+num2);
+    System.out.printf("Sum:           %d\n", num1 + num2);
   }
 }
