c106010b54f89bf1ae5886425d505270e0649d38
Elvin <elviuntaghiyev1842gmail.com>
2020-04-03 17:21:21 +0400

    initial commit

---
+++ src/main/java/module_02/lesson_09/HelloWorldApp.java
@@ -0,0 +1,10 @@
+package module_02.lesson_09;
+
+import java.io.PrintStream;
+
+public class HelloWorldApp {
+  public static void main(String[] args) {
+    PrintStream out = System.out;
+    out.println("Hello, World!");
+  }
+}
