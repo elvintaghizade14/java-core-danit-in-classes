4814dc9c5e94d5961b96465348835358735eb141
Elvin <elviuntaghiyev1842gmail.com>
2020-04-10 17:36:00 +0400

    initial commit

--- src/main/java/module_02/lesson_11/warmups/ShiftApp.java
+++ src/main/java/module_02/lesson_11/warmups/ShiftApp.java
@@ -11,23 +11,11 @@ public class ShiftApp {
     int toLeft = sc.nextInt();
     int toRight = sc.nextInt();
 
+    throw new IllegalArgumentException("Need to be implementing...");
     // 2. process
-    String ans = shift(s, toLeft, toRight);
+//    String ans = shift(s, toLeft, toRight);
 
     // 3. output
-    System.out.println(ans);
-  }
-
-  private static String shift(String s, int toLeft, int toRight) {
-    String res = (toLeft > 0 ? shiftLeft(s, toLeft) : shiftRight(s, Math.abs(toLeft)));
-    return (toRight > 0) ? shiftRight(res, toRight) : shiftLeft(res, Math.abs(toRight));
-  }
-
-  private static String shiftLeft(String s, int toLeft) {
-    return shiftRight(s, s.length() - toLeft);
-  }
-
-  private static String shiftRight(String s, int toRight) {
-    return String.format("%s%s", s.substring(toRight), s.substring(0, toRight));
+//    System.out.println(ans);
   }
 }
